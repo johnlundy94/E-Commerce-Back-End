2e93f9870bbe22e06fd20b7c777b54ae9e2c51a2
John Lundy <johnlundy94@gmail.com>
2021-09-20 16:34:25 -0600

    Updated routes and models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      equals: "10",
+      isNumeric: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("./config/connection");
+// import MySQL2
+const mysql = require("mysql2");
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  // MySQL username,
+  user: process.env.DB_USER,
+  // TODO: Add MySQL password here
+  password: process.env.DB_PASS,
+  database: process.env.NAME,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
